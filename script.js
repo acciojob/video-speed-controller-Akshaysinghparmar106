685d612be273e762770d7f684004b9f817e7da39
Akshaysinghparmar106 <parmarakshay973@gmail.com>
2026-01-06 20:38:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,60 +1,9 @@
-// const inputs = document.querySelectorAll('.controls input');
+const inputs = document.querySelectorAll('.controls input');
 
-//     function handleUpdate() {
-//       const suffix = this.dataset.sizing || '';
-//       document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-//     }
-
-//     inputs.forEach(input => input.addEventListener('change', handleUpdate));
-//     inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
-const video = document.getElementById("video");
-const toggleButton = document.getElementById("toggle");
-const progressBar = document.getElementById("progress");
-const progressFilled = document.getElementById("progress__filled");
-const volumeController = document.getElementById("volume");
-const playBackSpeedController = document.getElementById("playbackSpeed");
-const rewindButton = document.getElementById("rewind");
-const skipButton = document.getElementById("skip");
-
-toggleButton.addEventListener('click' , () => {
-    if(video.paused) {
-        video.play();
-        toggleButton.innerText = " ❚ ❚";
+    function handleUpdate() {
+      const suffix = this.dataset.sizing || '';
+      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
     }
-    else {
-        video.pause();
-        toggleButton.innerText = "►";
-    }
-})
 
-video.addEventListener("timeUpdate", () => {
-    const progress = (video.currentTime / video.duration) * 100;
-    progressBar.value = progress;
-    progressFilled.style.width = `${progress}%`;
-    console.log(progressFilled);
-})
-
-progressBar.addEventListener("input", () => {
-    const value = progressBar.value;
-    video.currentTime = (value/100) * video.duration;
-    console.log(video.currentTime);
-})
-
-volumeController.addEventListener("input", () => {
-    video.volume = volumeController.value;
-    console.log(video.volume);
-})
-
-playBackSpeedController.addEventListener("click", () => {
-    video.playbackRate = Number(playBackSpeedController.value);
-    console.dir(video);
-    console.log(video.playbackRate);
-})
-
-rewindButton.addEventListener("click", () => {
-    video.currentTime -= 10;
-})
-
-skipButton.addEventListener("click", () => {
-    video.currentTime += 25;
-})
+    inputs.forEach(input => input.addEventListener('change', handleUpdate));
+    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
